76f877ec8cfb7123452277cfa2a9398f0d7fe02f
Alexander Pérez-Ruiz <alexander.perez@escuelaing.edu.co>
2023-09-15 16:03:20 -0500

    Ciclo de lectura del archivo de entrada, terminado.

--- 230915_struct/main.cpp
+++ 230915_struct/main.cpp
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <fstream>
 
+struct complejo{
+  double re;
+  double img;
+};
 
-double norma_complejo(double re, double im){
-  double norma;
 
-  return norma;
+double norma_complejo(complejo c){
+  return sqrt( c.re * c.re  + c.img * c.img);
 }
 
-double angulo_complejo(double re, double im){
-  double angulo;
-
-  return angulo;
+double angulo_complejo(complejo c){
+  return atan2( c.img , c.re );
 }
 
 
@@ -23,9 +25,17 @@ int main(int argc, char** argv){
   }else{
     printf("Se abrirá el archivo %s\n", argv[1]);
     char buff[255];
+    // voy a crear un vector de complejos
+    complejo veccplx[50];
+    int i = 0;
     FILE * archivo = fopen( argv[1], "r" );
     while ( fgets(buff, 255, (FILE*) archivo )){
-      printf("%s", buff );
+      if( i%2 == 0 )
+        veccplx[i/2].re = strtod( buff, NULL );
+      else
+        veccplx[i/2].img = strtod( buff, NULL );
+
+      i++;
     }
   }
 
