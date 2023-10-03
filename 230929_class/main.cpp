e7fbcef9fba6f481765e405e3143c16566046bd2
Alexander Pérez-Ruiz <alexander.perez@escuelaing.edu.co>
2023-10-03 11:43:37 -0500

    Agregando las carpetas de las clases anteriores.

--- 230929_class/main.cpp
+++ 230929_class/main.cpp
@@ -2,21 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <fstream>
-
-struct complejo{
-  double re;
-  double img;
-};
+#include <complex>
+#include <iostream>
 
 
-double norma_complejo(complejo c){
-  return sqrt( c.re * c.re  + c.img * c.img);
-}
-
-double angulo_complejo(complejo c){
-  return atan2( c.img , c.re );
-}
-
+using namespace std;
 
 int main(int argc, char** argv){
   if( argc < 2){
@@ -26,23 +16,26 @@ int main(int argc, char** argv){
     printf("Se abrirá el archivo %s\n", argv[1]);
     char buff[255];
     // voy a crear un vector de complejos
-    complejo veccplx[50];
+    complex<double> veccplx[50];
     int i = 0;
     FILE * archivo = fopen( argv[1], "r" );
     while ( fgets(buff, 255, (FILE*) archivo )){
       if( i%2 == 0 )
-        veccplx[i/2].re = strtod( buff, NULL );
+        veccplx[i/2].real( strtod( buff, NULL ) );
       else
-        veccplx[i/2].img = strtod( buff, NULL );
+        veccplx[i/2].imag( strtod( buff, NULL ) );
 
       i++;
     }
     
    i = i/2;
    for( int j = 0; j<i; j++){
-
-
+     cout << veccplx[j] << endl; 
+     cout << "Magnitud: " << abs( veccplx[j] ) << " y ángulo " << arg( veccplx[j] ) << endl;
+     cout << "Su conjugado es: " << conj( veccplx[j] ) << endl;
+     cout << veccplx[j] * conj( veccplx[j] ) << endl;
    }
+
   }
 
   return 0;
