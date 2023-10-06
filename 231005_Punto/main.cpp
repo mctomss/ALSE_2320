52490dca6ba1570b127940e9c87c15daf1ed4bb1
Alexander Pérez-Ruiz <alexander.perez@escuelaing.edu.co>
2023-10-06 14:31:28 -0500

    En el main de 231005 se encuentra la tarea de hoy.

--- 231005_Punto/main.cpp
+++ 231005_Punto/main.cpp
@@ -62,5 +62,12 @@ int main(int argc, char** argv){
      operador división por un escalar.
   */
 
+
+  /* 231006: Una vez encontrado el centroide y la distancia al punto de test, 
+     listar los puntos del archivo que queden inscritos dentro de la circunferencia
+     con centro en test y radio = distancia(test, centroide). El archivo deberá 
+     contener al menos 50 puntos diferentes distribuidos uniformemente en el plano
+     XY.
+  */
   return 0;
 }
